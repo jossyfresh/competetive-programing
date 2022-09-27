854740c7ecd9e362679d06d5701c334115db3bee
jossyfresh <111150600+jossyfresh@users.noreply.github.com>
2022-09-27 11:13:44 -0700

    Create fizz buzz.py

---
+++ fizz buzz.py
@@ -0,0 +1,4 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        new_list = ["FizzBuzz" if x%3 == 0 and x%5==0 else "Fizz" if x%3==0 else "Buzz" if x%5==0 else str(x) for x in range(1,n+1)]
+        return new_list
